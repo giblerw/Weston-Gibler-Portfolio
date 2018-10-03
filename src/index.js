a96e5c77f57edbe1e7cb83f38f1ae877ec436f65
GiblerW <westongibler@gmail.com>
2018-10-03 13:45:42 -0600

    pre eject

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./global-styles";
 
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Container from "./components/Container";
 
 const App = () => (
